51704eb2ec57ba3ac663fbd59a81c718b7bc888e
Jun <wnsghd1@naver.com>
2023-11-11 04:19:28 +0900

    Plus One

--- Plus One.js
+++ Plus One.js
@@ -7,26 +7,48 @@
  */
 var plusOne = function(digits) {
 
-    let carry = true;
+    // let carry = true;
+    // let idx = digits.length - 1;
+
+    // while (carry){
+    //     const sum = digits[idx] + 1;
+
+    //     if(sum < 10) {
+    //         digits[idx] = sum;
+    //         carry = false;
+    //     }
+    //     else{
+    //         digits[idx] = 0;
+    //         if(idx === 0 ) {
+    //             digits.unshift(1);
+    //             carry = false;
+    //         }
+    //     }
+    //     idx--;
+    // }
+
+    // return digits
+
+    let carry = false;
     let idx = digits.length - 1;
 
-    while (carry){
-        const sum = digits[idx] + 1;
+    do {
+        const sum = (digits[idx] || 0) + 1;
 
-        if(sum < 10) {
-            digits[idx] = sum;
-            carry = false;
-        }
-        else{
+        if (sum >= 10) {
             digits[idx] = 0;
-            if(idx === 0 ) {
+            if (idx === 0) {
                 digits.unshift(1);
-                carry = false;
+                break;
             }
+        } else {
+            digits[idx] = sum;
+            break;
         }
+
         idx--;
-    }
+    } while (true);
 
-    return digits
-
+    return digits.slice();
+    
 };
