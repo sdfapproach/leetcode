12558322d0bec930fef78f7b543262a79d85bb8c
Jun <wnsghd1@naver.com>
2023-11-12 06:03:21 +0900

    Length of Last Word

---
+++ Length of Last Word.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/length-of-last-word/description/
+// 58. Length of Last Word
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let text = s;
+    text = text.trim();
+    text = text.split(" ");
+
+    return text[text.length-1].length
+
+};
