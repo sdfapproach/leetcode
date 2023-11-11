8da8bad56358d4f5ee552c16235b8638dbcbbab7
Jun <wnsghd1@naver.com>
2023-11-12 06:06:05 +0900

    Length of Last Word

--- Length of Last Word.js
+++ Length of Last Word.js
@@ -5,11 +5,14 @@
  * @param {string} s
  * @return {number}
  */
+// var lengthOfLastWord = function(s) {
+//     let text = s;
+//     text = text.trim();
+//     text = text.split(" ");
+
+//     return text[text.length-1].length
+
+// };
 var lengthOfLastWord = function(s) {
-    let text = s;
-    text = text.trim();
-    text = text.split(" ");
-
-    return text[text.length-1].length
-
+    return s.trim().split(" ")[s.trim().split(" ").length-1].length
 };
