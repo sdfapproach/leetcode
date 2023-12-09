a64f33ea40139aae536b741f08b43999707a3e63
Jun <wnsghd1@naver.com>
2023-12-10 01:00:55 +0900

    Largest Odd Number in String

--- Largest Odd Number in String.py
+++ Largest Odd Number in String.py
@@ -1,14 +1,27 @@
 # https://leetcode.com/problems/largest-odd-number-in-string/description/?envType=daily-question&envId=2023-12-07
 # 1903. Largest Odd Number in String
 
+#class Solution:
+#    def largestOddNumber(self, num: str) -> str:
+#        num = list(num)
+#        odd_num = ""
+
+#        for i, n in enumerate(reversed(num)):
+#            if int(n)%2 != 0:
+#                odd_num = num[:len(num)-i]
+#                break
+
+#        return "".join(odd_num)
+
 class Solution:
     def largestOddNumber(self, num: str) -> str:
-        num = list(num)
-        odd_num = ""
+        num = list(reversed(num))
+        odd_num = []
 
-        for i, n in enumerate(reversed(num)):
-            if int(n)%2 != 0:
-                odd_num = num[:len(num)-i]
+        for n in num:
+            if int(n) % 2 != 0:
+                odd_num.append(n)
+            else:
                 break
 
-        return "".join(odd_num)
+        return "".join(reversed(odd_num))
