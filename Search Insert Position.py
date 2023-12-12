72f4cea245340e3baf5e9ac014e7bd3106042fcd
Jun <wnsghd1@naver.com>
2023-12-12 22:48:57 +0900

    Search Insert Position

--- Search Insert Position.py
+++ Search Insert Position.py
@@ -1,10 +1,27 @@
 # https://leetcode.com/problems/search-insert-position/description/
 # Search Insert Position
 
+# class Solution:
+#     def searchInsert(self, nums: List[int], target: int) -> int:
+#         for i, n in enumerate(nums):
+#             if n >= target:
+#                 return i
+
+#         return len(nums)
+
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        for i, n in enumerate(nums):
-            if n >= target:
-                return i
+        left, right = 0, len(nums) - 1
 
-        return len(nums)
+        while left <= right:
+            mid = (left + right) // 2
+            mid_value = nums[mid]
+
+            if mid_value == target:
+                return mid
+            elif mid_value < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
