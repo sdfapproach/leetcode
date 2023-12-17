994cd8d044455686267951b1bd1443f5eb130476
Jun <wnsghd1@naver.com>
2023-12-18 02:11:38 +0900

    Destination City

---
+++ Destination City.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/destination-city/description/?envType=daily-question&envId=2023-12-15
+# Destination City
+
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+
+        dep = [dep[0] for dep in paths]
+        des = [des[1] for des in paths]
+
+        for city in des:
+            if city not in dep:
+                return city
