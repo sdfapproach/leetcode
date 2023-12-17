7c206750703b1acd46ace0e6244b53ab08cbc50c
Jun <wnsghd1@naver.com>
2023-12-17 17:11:42 +0900

    Valid Anagram

--- Valid Anagram.py
+++ Valid Anagram.py
@@ -1,7 +1,13 @@
 # https://leetcode.com/problems/valid-anagram/description/?envType=daily-question&envId=2023-12-16
 # Valid Anagram
 
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+        
+#        return sorted(list(s)) == sorted(list(t))
+
+from collections import Counter
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-       return sorted(list(s)) == sorted(list(t))
+        return Counter(s) == Counter(t)
