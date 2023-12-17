31536f7bcd3502c46423d624d42fee95e49199b3
Jun <wnsghd1@naver.com>
2023-12-18 02:16:28 +0900

    Destination City

--- Destination City.py
+++ Destination City.py
@@ -4,8 +4,14 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
 
-        dep = [dep[0] for dep in paths]
-        des = [des[1] for des in paths]
+        # dep = [dep[0] for dep in paths]
+        # des = [des[1] for des in paths]
+
+        # for city in des:
+        #     if city not in dep:
+        #         return city
+
+        dep, des = zip(*paths)
 
         for city in des:
             if city not in dep:
