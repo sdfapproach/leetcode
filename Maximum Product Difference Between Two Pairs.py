9bb253cc8fdb9cb58d0e64cc71083398ae9c4a3f
Jun <wnsghd1@naver.com>
2023-12-19 22:29:09 +0900

    Maximum Product Difference Between Two Pairs

--- Maximum Product Difference Between Two Pairs.py
+++ Maximum Product Difference Between Two Pairs.py
@@ -3,6 +3,22 @@
 
 class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
-        new_list = sorted(nums)
+        # new_list = sorted(nums)
 
-        return new_list[-1]*new_list[-2] - new_list[0]*new_list[1]
+        # return new_list[-1]*new_list[-2] - new_list[0]*new_list[1]
+
+        max1, max2 = float('-inf'), float('-inf')
+        min1, min2 = float('inf'), float('inf')
+
+        for num in nums:
+            if num > max1:
+                max2, max1 = max1, num
+            elif num > max2:
+                max2 = num
+
+            if num < min1:
+                min2, min1 = min1, num
+            elif num < min2:
+                min2 = num
+
+        return max1 * max2 - min1 * min2
