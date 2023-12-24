e87de37ae5463dbbef59c1357fee1eb704ebbc71
Jun <wnsghd1@naver.com>
2023-12-25 03:27:46 +0900

    Path Crossing

---
+++ Path Crossing.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/path-crossing/description/?envType=daily-question&envId=2023-12-23
+# Path Crossing
+
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+
+        point = [0, 0]
+        points = [[0, 0]]
+
+        def move(d):
+            new_point = []
+            if d == 'N':
+                new_point.append(point[0])
+                new_point.append(point[1] + 1)
+            if d == 'S':
+                new_point.append(point[0])
+                new_point.append(point[1] - 1)
+            if d == 'E':
+                new_point.append(point[0] + 1)
+                new_point.append(point[1])
+            if d == 'W':
+                new_point.append(point[0] - 1)
+                new_point.append(point[1])
+            points.append(new_point)
+            point[0] = new_point[0]
+            point[1] = new_point[1]
+
+        for m in path:
+            move(m)
+            for item in points[:len(points)-1]:
+                print(item, point)
+                if item[0] == point[0] and item[1] == point[1]:
+                    return True
+
+        return False
