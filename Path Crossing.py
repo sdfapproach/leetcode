875695d8ee2d039cc3dba476124f574e49655e73
Jun <wnsghd1@naver.com>
2023-12-25 03:29:24 +0900

    Path Crossing

--- Path Crossing.py
+++ Path Crossing.py
@@ -3,33 +3,48 @@
 
 class Solution:
     def isPathCrossing(self, path: str) -> bool:
+        # point = [0, 0]
+        # points = [[0, 0]]
 
-        point = [0, 0]
-        points = [[0, 0]]
+        # def move(d):
+        #     new_point = []
+        #     if d == 'N':
+        #         new_point.append(point[0])
+        #         new_point.append(point[1] + 1)
+        #     if d == 'S':
+        #         new_point.append(point[0])
+        #         new_point.append(point[1] - 1)
+        #     if d == 'E':
+        #         new_point.append(point[0] + 1)
+        #         new_point.append(point[1])
+        #     if d == 'W':
+        #         new_point.append(point[0] - 1)
+        #         new_point.append(point[1])
+        #     points.append(new_point)
+        #     point[0] = new_point[0]
+        #     point[1] = new_point[1]
 
-        def move(d):
-            new_point = []
-            if d == 'N':
-                new_point.append(point[0])
-                new_point.append(point[1] + 1)
-            if d == 'S':
-                new_point.append(point[0])
-                new_point.append(point[1] - 1)
-            if d == 'E':
-                new_point.append(point[0] + 1)
-                new_point.append(point[1])
-            if d == 'W':
-                new_point.append(point[0] - 1)
-                new_point.append(point[1])
-            points.append(new_point)
-            point[0] = new_point[0]
-            point[1] = new_point[1]
+        # for m in path:
+        #     move(m)
+        #     for item in points[:len(points)-1]:
+        #         print(item, point)
+        #         if item[0] == point[0] and item[1] == point[1]:
+        #             return True
 
-        for m in path:
-            move(m)
-            for item in points[:len(points)-1]:
-                print(item, point)
-                if item[0] == point[0] and item[1] == point[1]:
-                    return True
+        # return False
+
+        point = (0, 0)
+        points = {point}
+
+        directions = {'N': (0, 1), 'S': (0, -1), 'E': (1, 0), 'W': (-1, 0)}
+
+        for move in path:
+            dx, dy = directions[move]
+            point = (point[0] + dx, point[1] + dy)
+
+            if point in points:
+                return True
+
+            points.add(point)
 
         return False
