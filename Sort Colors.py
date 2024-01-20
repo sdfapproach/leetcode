cf7fd8c3a80b881375de38734f743be922fca8e3
Jun <wnsghd1@naver.com>
2024-01-21 00:04:11 +0900

    Sort Colors

---
+++ Sort Colors.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/sort-colors/
+# Sort Colors
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
