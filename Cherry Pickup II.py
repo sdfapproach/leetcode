a6adcdcbe026dbbc1ba7e3bc0fb0bd440e0336d4
Jun <wnsghd1@naver.com>
2024-02-12 04:57:16 +0900

    Cherry Pickup II

---
+++ Cherry Pickup II.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/cherry-pickup-ii/?envType=daily-question&envId=2024-02-11
+# Cherry Pickup II
+
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+    
+        dp = [[[None for _ in range(cols)] for _ in range(cols)] for _ in range(rows)]
+
+        def dfs(row, col1, col2):
+            if col1 < 0 or col1 >= cols or col2 < 0 or col2 >= cols:
+                return 0
+            if dp[row][col1][col2] is not None:
+                return dp[row][col1][col2]
+            
+            result = grid[row][col1]
+            if col1 != col2:
+                result += grid[row][col2]
+            
+            if row != rows - 1:
+                max_cherries = 0
+                for new_col1 in (col1-1, col1, col1+1):
+                    for new_col2 in (col2-1, col2, col2+1):
+                        max_cherries = max(max_cherries, dfs(row + 1, new_col1, new_col2))
+                result += max_cherries
+            
+            dp[row][col1][col2] = result
+            return result
+        
+        return dfs(0, 0, cols - 1)
