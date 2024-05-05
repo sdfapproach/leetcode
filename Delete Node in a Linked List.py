3a7506f6a042605fba2501a3349821a32e5a29c9
Jun <wnsghd1@naver.com>
2024-05-06 04:53:37 +0900

    Delete Node in a Linked List

---
+++ Delete Node in a Linked List.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/delete-node-in-a-linked-list/?envType=daily-question&envId=2024-05-05
+# Delete Node in a Linked List
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+
+        node.val = node.next.val
+        node.next = node.next.next
