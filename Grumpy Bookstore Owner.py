421e830227650ad108b45dc0485c8fe7591abf4e
Jun <wnsghd1@naver.com>
2024-06-22 18:10:56 +0900

    Grumpy Bookstore Owner

---
+++ Grumpy Bookstore Owner.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/grumpy-bookstore-owner/?envType=daily-question&envId=2024-06-21
+# Grumpy Bookstore Owner
+
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+
+        n = len(customers)
+        
+        base_satisfied = 0
+
+        for i in range(n):
+            if grumpy[i] == 0:
+                base_satisfied += customers[i]
+        
+        max_additional_satisfied = 0
+        current_additional_satisfied = 0
+        
+        for i in range(n):
+            if grumpy[i] == 1:
+                current_additional_satisfied += customers[i]
+            
+            if i >= minutes:
+                if grumpy[i - minutes] == 1:
+                    current_additional_satisfied -= customers[i - minutes]
+            
+            max_additional_satisfied = max(max_additional_satisfied, current_additional_satisfied)
+        
+        return base_satisfied + max_additional_satisfied
