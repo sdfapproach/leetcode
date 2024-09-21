f974fbca54931cf467a8a55cac778c340d54f365
Jun <wnsghd1@naver.com>
2024-09-22 04:32:49 +0900

    Lexicographical Numbers

---
+++ Lexicographical Numbers.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/lexicographical-numbers/?envType=daily-question&envId=2024-09-21
+# Lexicographical Numbers
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        
+        result = []
+        current = 1
+
+        for _ in range(n):
+            result.append(current)
+            if current * 10 <= n:
+                current *= 10
+            else:
+                if current >= n:
+                    current //= 10
+                current += 1
+                while current % 10 == 0:
+                    current //= 10
+
+        return result
