86e7ef9b117d707a88ed74fb8ffc71374483abbe
Jun <wnsghd1@naver.com>
2024-11-08 18:14:01 +0900

    Largest Combination With Bitwise AND Greater Than Zero

---
+++ Largest Combination With Bitwise AND Greater Than Zero.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/largest-combination-with-bitwise-and-greater-than-zero/?envType=daily-question&envId=2024-11-07
+# Largest Combination With Bitwise AND Greater Than Zero
+
+class Solution:
+    def largestCombination(self, candidates: List[int]) -> int:
+        
+        max_count = 0
+
+        for bit in range(24):
+            count = 0
+            for num in candidates:
+                if num & (1 << bit):
+                    count += 1
+            max_count = max(max_count, count)
+
+        return max_count
