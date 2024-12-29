1bf264cc6a92f8b76c635cb07009411d65d5cea1
Jun <wnsghd1@naver.com>
2024-12-30 01:39:16 +0900

    Number of Ways to Form a Target String Given a Dictionary

---
+++ Number of Ways to Form a Target String Given a Dictionary.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/number-of-ways-to-form-a-target-string-given-a-dictionary/?envType=daily-question&envId=2024-12-29
+# Number of Ways to Form a Target String Given a Dictionary
+
+class Solution:
+    def numWays(self, words: List[str], target: str) -> int:
+
+        @cache
+        def dfs(i: int, j: int) -> int:
+
+            if i >= m:
+                return 1
+
+            if j >= n:
+                return 0
+
+            ans = dfs(i + 1, j + 1) * cnt[j][ord(target[i]) - ord('a')]
+            ans = (ans + dfs(i, j + 1)) % mod
+
+            return ans
+
+        m, n = len(target), len(words[0])
+        cnt = [[0] * 26 for _ in range(n)]
+
+        for w in words:
+            for j, c in enumerate(w):
+                cnt[j][ord(c) - ord('a')] += 1
+
+        mod = 10**9 + 7
+
+        return dfs(0, 0)
