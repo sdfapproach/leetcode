5797f020a4889da9d5207119488d149d07389c4c
Jun <wnsghd1@naver.com>
2025-01-30 02:04:05 +0900

    Redundant Connection

---
+++ Redundant Connection.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/redundant-connection/?envType=daily-question&envId=2025-01-29
+# Redundant Connection
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n + 1))
+        self.rank = [1] * (n + 1)
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX == rootY:
+            return False
+        if self.rank[rootX] > self.rank[rootY]:
+            self.parent[rootY] = rootX
+        elif self.rank[rootX] < self.rank[rootY]:
+            self.parent[rootX] = rootY
+        else:
+            self.parent[rootY] = rootX
+            self.rank[rootX] += 1
+        return True
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        
+        n = len(edges)
+        uf = UnionFind(n)
+
+        for edge in edges:
+            u, v = edge
+            if not uf.union(u, v):
+                return edge
