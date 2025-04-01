dbd8fc2378e9c8fff9b96b8710ea47fe1c5f87a1
Jun <wnsghd1@naver.com>
2025-04-02 02:04:11 +0900

    Solving Questions With Brainpower

---
+++ Solving Questions With Brainpower.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/solving-questions-with-brainpower/?envType=daily-question&envId=2025-04-01
+# Solving Questions With Brainpower
+
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        
+        n = len(questions)
+        dp = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            points, brainpower = questions[i]
+            next_index = i + brainpower + 1
+            solve = points + (dp[next_index] if next_index < n else 0)
+            skip = dp[i + 1]
+            dp[i] = max(solve, skip)
+        
+        return dp[0]
