d9b87efbc8bc53dbbacbf95162214998039daf1e
Jun <wnsghd1@naver.com>
2025-04-06 08:42:04 +0900

    Sum of All Subset XOR Totals

---
+++ Sum of All Subset XOR Totals-2.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/sum-of-all-subset-xor-totals/?envType=daily-question&envId=2025-04-05
+# Sum of All Subset XOR Totals
+
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+
+        def helper(idx, current_xor):
+            nonlocal total
+            if idx == len(nums):
+                total += current_xor
+                return
+
+            helper(idx + 1, current_xor ^ nums[idx])
+            helper(idx + 1, current_xor)
+        
+        total = 0
+        helper(0, 0)
+        return total
