4d8e9f47428b39b381e5d12b28844667f142a2fd
Jun <wnsghd1@naver.com>
2025-05-03 03:33:04 +0900

    Push Dominoes

---
+++ Push Dominoes.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/push-dominoes/?envType=daily-question&envId=2025-05-02
+# Push Dominoes
+
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        
+        n = len(dominoes)
+        q = deque()
+        time = [-1] * n
+        force = defaultdict(list)
+        for i, f in enumerate(dominoes):
+            if f != '.':
+                q.append(i)
+                time[i] = 0
+                force[i].append(f)
+        ans = ['.'] * n
+        while q:
+            i = q.popleft()
+            if len(force[i]) == 1:
+                ans[i] = f = force[i][0]
+                j = i - 1 if f == 'L' else i + 1
+                if 0 <= j < n:
+                    t = time[i]
+                    if time[j] == -1:
+                        q.append(j)
+                        time[j] = t + 1
+                        force[j].append(f)
+                    elif time[j] == t + 1:
+                        force[j].append(f)
+        return ''.join(ans)
