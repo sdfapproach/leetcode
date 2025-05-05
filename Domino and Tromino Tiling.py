a1ccec8ef99113716d8bb85e4bc7179e1a95b095
Jun <wnsghd1@naver.com>
2025-05-06 05:42:18 +0900

    Domino and Tromino Tiling

---
+++ Domino and Tromino Tiling.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/domino-and-tromino-tiling/?envType=daily-question&envId=2025-05-05
+# Domino and Tromino Tiling
+
+class Solution:
+    def numTilings(self, n: int) -> int:
+        
+        f = [1, 0, 0, 0]
+        mod = 10**9 + 7
+
+        for i in range(1, n + 1):
+            g = [0] * 4
+            g[0] = (f[0] + f[1] + f[2] + f[3]) % mod
+            g[1] = (f[2] + f[3]) % mod
+            g[2] = (f[1] + f[3]) % mod
+            g[3] = f[0]
+            f = g
+
+        return f[0]
