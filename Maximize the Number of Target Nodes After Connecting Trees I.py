61a042ae34b72a93f9b8a46525d3d04228b6c130
Jun <wnsghd1@naver.com>
2025-05-29 03:21:21 +0900

    Maximize the Number of Target Nodes After Connecting Trees I

---
+++ Maximize the Number of Target Nodes After Connecting Trees I.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/maximize-the-number-of-target-nodes-after-connecting-trees-i/description/?envType=daily-question&envId=2025-05-28
+# Maximize the Number of Target Nodes After Connecting Trees I
+
+class Solution:
+    def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]], k: int) -> List[int]:
+        
+        def build(edges: List[List[int]]) -> List[List[int]]:
+            n = len(edges) + 1
+            g = [[] for _ in range(n)]
+            for a, b in edges:
+                g[a].append(b)
+                g[b].append(a)
+            return g
+
+        def dfs(g: List[List[int]], a: int, fa: int, d: int) -> int:
+            if d < 0:
+                return 0
+            cnt = 1
+            for b in g[a]:
+                if b != fa:
+                    cnt += dfs(g, b, a, d - 1)
+            return cnt
+
+        g2 = build(edges2)
+        m = len(edges2) + 1
+        t = max(dfs(g2, i, -1, k - 1) for i in range(m))
+        g1 = build(edges1)
+        n = len(edges1) + 1
+
+        return [dfs(g1, i, -1, k) + t for i in range(n)]
