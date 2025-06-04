5aea085ba86e79207291cd3d98bf60c5e9ea8164
Jun <wnsghd1@naver.com>
2025-06-05 02:06:32 +0900

    Find the Lexicographically Largest String From the Box I

---
+++ Find the Lexicographically Largest String From the Box I.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/find-the-lexicographically-largest-string-from-the-box-i/?envType=daily-question&envId=2025-06-04
+# Find the Lexicographically Largest String From the Box I
+
+class Solution:
+    def answerString(self, word: str, numFriends: int) -> str:
+        
+        if numFriends == 1:
+            return word
+        s = self.lastSubstring(word)
+
+        return s[: len(word) - numFriends + 1]
+
+    def lastSubstring(self, s: str) -> str:
+        i, j, k = 0, 1, 0
+        while j + k < len(s):
+            if s[i + k] == s[j + k]:
+                k += 1
+            elif s[i + k] < s[j + k]:
+                i += k + 1
+                k = 0
+                if i >= j:
+                    j = i + 1
+            else:
+                j += k + 1
+                k = 0
+        return s[i:]
