f20a2704e36fed29f410330baca296101f3e599b
Jun <wnsghd1@naver.com>
2025-07-16 01:46:09 +0900

    Valid Word

---
+++ Valid Word.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/valid-word/?envType=daily-question&envId=2025-07-15
+# Valid Word
+
+class Solution:
+    def isValid(self, word: str) -> bool:
+        
+        if len(word) < 3:
+            return False
+        
+        vowels = set("aeiouAEIOU")
+        has_vowel = False
+        has_consonant = False
+        
+        for ch in word:
+            if not ch.isalnum():
+                return False
+            
+            if ch.isalpha():
+                if ch in vowels:
+                    has_vowel = True
+                else:
+                    has_consonant = True
+        
+        return has_vowel and has_consonant
