f55796ce1d5ed533a82a428fbd791edba4a1b6c2
Jun <wnsghd1@naver.com>
2025-08-16 02:16:50 +0900

    Power of Four

---
+++ Power of Four.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/power-of-four/?envType=daily-question&envId=2025-08-15
+# Power of Four
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        
+        if n < 1:
+            return False
+
+        while n % 4 == 0:
+            n //= 4
+
+        return n == 1
