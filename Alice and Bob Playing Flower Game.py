cb6eb864423620f0e63e79a090344ffc32089f1e
Jun <wnsghd1@naver.com>
2025-08-30 04:40:24 +0900

    Alice and Bob Playing Flower Game

---
+++ Alice and Bob Playing Flower Game.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/alice-and-bob-playing-flower-game/?envType=daily-question&envId=2025-08-29
+# Alice and Bob Playing Flower Game
+
+class Solution:
+    def flowerGame(self, n: int, m: int) -> int:
+
+        return (n * m) // 2
