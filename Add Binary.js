036e2c879d3055232d8b9fd3c53ce29c084f8e81
Jun <wnsghd1@naver.com>
2026-02-16 23:33:42 +0900

    Add Binary

---
+++ Add Binary.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/add-binary/?envType=daily-question&envId=2026-02-15
+// Add Binary
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+
+    let i = a.length - 1;
+    let j = b.length - 1;
+    let carry = 0;
+    let result = [];
+
+    while (i >= 0 || j >= 0 || carry) {
+        let sum = carry;
+
+        if (i >= 0) sum += a[i--] - '0';
+        if (j >= 0) sum += b[j--] - '0';
+
+        result.push(sum % 2);
+        carry = Math.floor(sum / 2);
+    }
+
+    return result.reverse().join('');
+};
