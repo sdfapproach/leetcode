6de9acfae7286d0c3aded157c5ee8bdf802a6868
Jun <wnsghd1@naver.com>
2026-07-27 01:46:15 +0900

    Maximum Product of Three Numbers

---
+++ Maximum Product of Three Numbers.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/maximum-product-of-three-numbers/?envType=daily-question&envId=2026-07-26
+# Maximum Product of Three Numbers
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        
+        nums.sort()
+
+        return max(nums[0] * nums[1] * nums[-1], nums[-1] * nums[-2] * nums[-3])
