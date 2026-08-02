ba2d07a56460e543ea71ae3d67674ec67566dbbd
Jun <wnsghd1@naver.com>
2026-08-03 01:34:36 +0900

    Stone Game

---
+++ Stone Game.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/stone-game/?envType=daily-question&envId=2026-08-02
+# Stone Game
+
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        
+        return True
