dc28fdae33eba9648e8214e00e63b716e6fa277c
Jun <wnsghd1@naver.com>
2026-08-17 03:39:06 +0900

    Stone Game IX

---
+++ Stone Game IX.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/stone-game-ix/?envType=daily-question&envId=2026-08-16
+# Stone Game IX
+
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        
+        cnt = [0, 0, 0]
+
+        for stone in stones:
+            cnt[stone % 3] += 1
+
+        if cnt[0] % 2 == 0:
+            return cnt[1] > 0 and cnt[2] > 0
+
+        return abs(cnt[1] - cnt[2]) > 2
