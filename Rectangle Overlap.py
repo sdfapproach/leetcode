c4e6f540da33a1ad94c72cee6c86ea02fe8b068d
Jun <wnsghd1@naver.com>
2026-09-15 02:57:22 +0900

    Rectangle Overlap

---
+++ Rectangle Overlap.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/rectangle-overlap/?envType=daily-question&envId=2026-09-14
+# Rectangle Overlap
+
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        
+         return (
+            rec1[0] < rec2[2] and
+            rec2[0] < rec1[2] and
+            rec1[1] < rec2[3] and
+            rec2[1] < rec1[3]
+        )
